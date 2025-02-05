e327de21581f030eb0140b9bc4620893f8a57cab
to236663 <tommyfernandez099@gmail.com>
2025-02-04 23:43:17 -0500

    react app working changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,26 @@
 import { useState, useCallback } from "react"
-/**
- * Renders an array of strings passed in that can be filtered and added to as an
- * unordered list.
- * @returns Component
- */
-export default function Sidebar() {
+
+export default function Sidebar({initialMenuItems}) {
   let [newMenuItem, setNewMenuItem] = useState("")
-  // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
-  // Adds a single string passed in as parameter to the state element
-  // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
-    console.log("Added menu item")
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
+    if (newMenuItem.trim () !== "") {
+      setMenuItems((prevMenuItems) => [...prevMenuItems, newMenuItem]);
+      setNewMenuItem(""); 
+      console.log("Added menu item")
+    }
+  }, [newMenuItem])
 
-  // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
+  let filteredMenuItems = menuItems.filter((item) => item.toLowerCase().includes(filter.toLowerCase()))
 
-  // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
-  // its own item.
   return (
     <div>
+      <ul>
+        {filteredMenuItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}  
+      </ul>
       <input
         type="text"
         id="newMenuItemValue"
@@ -32,13 +28,7 @@ export default function Sidebar() {
         onChange={(event) => setNewMenuItem(event.target.value)}
       ></input>
       <br />
-      <button
-        onClick={() => {
-          /* TODO: 3 */
-        }}
-      >
-        Add Item
-      </button>
+      <button onClick={addMenuItem}>Add Item</button>
       <br />
       <input
         id="filter"
